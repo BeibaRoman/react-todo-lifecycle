32a76f42b0ea005e0db9048ba5613158bc88e990
Beiba Roman <106346781+BeibaRoman@users.noreply.github.com>
2026-08-13 15:23:41 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <main className={styles.page}>
+        <div className="page__container">
+          <h1 className={styles.title}>React Homework Template</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+export default App;
